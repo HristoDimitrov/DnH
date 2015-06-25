28e133140d3e57ebe801c39b7e72ffef04b88820
Didoni <dk8921@bristol.ac.uk>
2015-06-25 14:13:59 +0100

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
 $('input#name-submit').on('click', function () {
 	var name = $('input#name').val();
 	$.ajax({ 
-            url: 'ajax/name.php',
+            url: 'http://hristodimitrov.github.io/DnH/ajax/name.php',
             type: 'POST',
             dataType: 'text',
             data: {
