372f88bbda58980411ea54545b6848e2ecda74af
Didoni <dk8921@bristol.ac.uk>
2015-06-25 13:31:41 +0100

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
 $('input#name-submit').on('click', function () {
-    var name = $('input#name').val();
-	$.post('ajax/name.php', {name:name}, function(data) {	
-        alert(data);
+	var name = $('input#name').val();
+	$.post('ajax/name.php', {name:name}, function(name) {	
+        	alert(1);
 	});
  
 });
