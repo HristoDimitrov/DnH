66013cad9e393a78c477db4559c3055ba1d4e345
Didoni <dk8921@bristol.ac.uk>
2015-06-25 13:36:14 +0100

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,6 +1,6 @@
 $('input#name-submit').on('click', function () {
 	var name = $('input#name').val();
-	$.post('https://github.com/HristoDimitrov/DnH/blob/gh-pages/ajax/name.php', {name:name}, function(data) {	
+	$.post('http://hristodimitrov.github.io/DnH/ajax/name.php', {name:name}, function(data) {	
         	alert(1);
 	});
  
